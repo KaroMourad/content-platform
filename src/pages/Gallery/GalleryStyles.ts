c13dafa38f8fbf1458d113890b9cbfe1f14c7ab3
Karo <karapetmouradyan@gmail.com>
2024-08-27 11:46:22 +0400

    fix: gallery styles

--- src/pages/Gallery/GalleryStyles.ts
+++ src/pages/Gallery/GalleryStyles.ts
@@ -4,6 +4,8 @@ const useStyles = createUseStyles({
   gallery: {
     height: "100%",
     width: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
   gridItemClass: {
     padding: "var(--spacing-xxxs, 0.125rem)",
@@ -22,26 +24,26 @@ const useStyles = createUseStyles({
     position: "relative",
     overflow: "hidden",
     height: "100%",
-    '&:hover $imageTitle': {
+    "&:hover $imageTitle": {
       opacity: 1,
-      display: 'block'
+      display: "block",
     },
   },
   imageTitle: {
-    transition: 'opacity 0.2s ease-in-out',
+    transition: "opacity 0.2s ease-in-out",
     opacity: 0,
     position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
-    padding: 'var(--spacing-xs, 0.5rem) var(--spacing-md, 1rem)',
+    padding: "var(--spacing-xs, 0.5rem) var(--spacing-md, 1rem)",
     boxShadow: "inset 0 -2em 1em 0 rgba(0, 0, 0, 0.6)",
     color: "#fff",
     fontSize: "var(--font-size-xs, 0.75rem)",
     textAlign: "center",
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
   },
 });
 
